21e1f6fb76bed2ea1a0e0446511448fd77a4dd0f
sinpu <sinpu11080@gmail.com>
2016-06-16 12:03:53 +0900

    適切なパネルの切り離し

--- src/window.java
+++ src/window.java
@@ -72,48 +72,8 @@ public class window {
 		splitPane.setRightComponent(panel);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
-		JSplitPane splitPane_1 = new JSplitPane();
-		splitPane_1.setOrientation(JSplitPane.VERTICAL_SPLIT);
-		panel.add(splitPane_1);
 		
-	
-		JLabel lblNewLabel = new JLabel("Image Test",new ImageIcon("/home/sinpu/ダウンロード/kadai02.png"),JLabel.CENTER);
-		splitPane_1.setLeftComponent(lblNewLabel);
-		
-		JPanel panel_2 = new JPanel();
-		splitPane_1.setRightComponent(panel_2);
-		panel_2.setLayout(new GridLayout(2, 4, 0, 0));
-		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Answer00");
-		panel_2.add(rdbtnNewRadioButton);
-		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Answer01");
-		panel_2.add(rdbtnNewRadioButton_1);
-		
-		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Answer02");
-		panel_2.add(rdbtnNewRadioButton_2);
-		
-		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("Answer03");
-		panel_2.add(rdbtnNewRadioButton_3);
-		
-		JRadioButton rdbtnNewRadioButton_4 = new JRadioButton("Answer04");
-		panel_2.add(rdbtnNewRadioButton_4);
-		
-		JRadioButton rdbtnNewRadioButton_5 = new JRadioButton("Answer05");
-		panel_2.add(rdbtnNewRadioButton_5);
-		
-		JRadioButton rdbtnNewRadioButton_6 = new JRadioButton("Answer06");
-		panel_2.add(rdbtnNewRadioButton_6);
-		
-		JRadioButton rdbtnNewRadioButton_7 = new JRadioButton("Answer07");
-		panel_2.add(rdbtnNewRadioButton_7);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setHorizontalAlignment(SwingConstants.RIGHT);
-		btnNewButton.setVerticalAlignment(SwingConstants.BOTTOM);
-		panel.add(btnNewButton);
-		
-
+		//menu view
 		JPanel panel_1 = new JPanel();
 		splitPane.setLeftComponent(panel_1);
 		
