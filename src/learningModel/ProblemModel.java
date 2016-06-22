8ab994fa6c477be567c79761b79142599eee4af6
sinpu <sinpu11080@gmail.com>
2016-06-23 03:49:31 +0900

    問題リストを基礎に、問題の管理を行う。

---
+++ src/learningModel/ProblemModel.java
@@ -0,0 +1,23 @@
+package learningModel;
+
+import java.util.Map;
+
+public class ProblemModel {
+
+	//select answer
+	//private Map<String, Integer> answerList = new Map<String, Integer>();
+	private String[] answerList = new String[8];
+	//problem
+	private String problemImageURI = new String();
+	
+	//answer
+	private String[] answerImageURI = new String[8];
+	
+	public void setAnser(){
+		
+	}
+	
+	public String[] getAnser(){
+		return answerList;
+	}
+}
