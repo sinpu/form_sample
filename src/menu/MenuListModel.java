8438b5b1240ce6609f7b37edd7befb45aa113fff
sinpu <sinpu11080@gmail.com>
2016-06-23 09:33:51 +0900

    メニューの改定

--- src/menu/MenuListModel.java
+++ src/menu/MenuListModel.java
@@ -9,6 +9,8 @@ import javax.swing.event.EventListenerList;
 
 import org.eclipse.ui.internal.handlers.WizardHandler.New;
 
+import window.MainWindow;
+
 import main.frame.TransitionModel;
 
 public class MenuListModel extends DefaultListModel<Object> implements ActionListener{
@@ -16,21 +18,43 @@ public class MenuListModel extends DefaultListModel<Object> implements ActionLis
 	// protected EventListenerList listenerList
 	//private String[] list = {TransitionModel.Home.toString(),"PROPERTY","EXIT"};
 	
+	private MainWindow mainWindow;
+	
 	public MenuListModel(){
 		super();
 		init();
 	}
 	
-	private void init(){
+	public MenuListModel(MainWindow mainFrame){
+		super();
 		
+		mainWindow = mainFrame;
+	}
+	
+	public void init(){
 		for(TransitionModel data : TransitionModel.values() ){
-			this.addElement(data.toString());
+			addElement(data.toString());
+		}		
+	}
+	
+	
+	public JButton[] init2(){
+		JButton[] jButtons = new JButton[TransitionModel.values().length];
+		
+		int i = 0;
+		for(TransitionModel data : TransitionModel.values() ){
+			jButtons[i] = new JButton(data.toString());
 		}
+		
+		return jButtons;
+		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		String command = e.getActionCommand();
+		mainWindow.changePanel(TransitionModel.valueOf(command));
 	}
 	
 	
--- src/window/MainWindow.java
+++ src/window/MainWindow.java
@@ -1,5 +1,6 @@
 package window;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Paint;
@@ -37,6 +38,8 @@ import javax.swing.JTextField;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
@@ -112,12 +115,25 @@ public class MainWindow extends JFrame{
 		
 		//menu view
 		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
 		splitPane.setLeftComponent(panel_1);
 		
+		MenuListModel menu = new MenuListModel(this);
+		JButton[] menuBtn = new JButton[TransitionModel.values().length];
+		int i = 0;
+		for(TransitionModel data : TransitionModel.values()){
+			menuBtn[i] = new JButton(data.toString()); 
+			menuBtn[i].setMaximumSize(new Dimension(Short.MAX_VALUE, menuBtn[i].getMaximumSize().height));
+			menuBtn[i].addActionListener(menu);
+			panel_1.add(menuBtn[i]);
+			i++;
+		}
+		
+		/* 使って見たが、listenerが理想的ではない
 		JList list = new JList();
 		list.setModel(new MenuListModel());
 		panel_1.add(list);
-		
+		*/
 	}
 	
 	public void startSystem(String id,String pass){
