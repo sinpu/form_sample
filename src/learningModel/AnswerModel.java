ad8077edc0f8e04e9cc56a53b6ad82172d2a18e0
sinpu <sinpu11080@gmail.com>
2016-07-15 06:01:55 +0900

    コンストラクタ設定

--- src/learningModel/AnswerModel.java
+++ src/learningModel/AnswerModel.java
@@ -1,7 +1,13 @@
 package learningModel;
 
 public class AnswerModel {
-	String Answer;
+	String answer;
 	boolean check;
 	boolean[] diagnostic = new boolean[24];
+	
+	AnswerModel(String answer, boolean check, boolean[] diagnostic){
+		this.answer = answer;
+		this.check = check;
+		this.diagnostic = diagnostic;
+	}
 }
